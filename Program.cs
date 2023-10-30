d98243f250d142161f35800a560b1655031eb8df
Favio Valero <favio102@protonmail.com>
2023-10-30 04:18:54 -0700

    challenge activity to apply business rules

--- Program.cs
+++ Program.cs
@@ -52,37 +52,62 @@
 // }
 // -----------Decision logic game 2nd version
 
-Random dice = new Random();
+// Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
 
-// // test the code
-// roll1 = 4;
-// roll2 = 2;
-// roll3 = 1;
+// // // test the code
+// // roll1 = 4;
+// // roll2 = 2;
+// // roll3 = 1;
 
-int total = roll1 + roll2 + roll3;
+// int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// // Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
-  if ((roll1 == roll2) && (roll2 == roll3)) {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-  } else {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-  }
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+//   if ((roll1 == roll2) && (roll2 == roll3)) {
+//     // Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+//   } else {
+//     // Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+//   }
+// }
+
+// if (total >= 16) {
+//   // Console.WriteLine("You win a car!");
+//   } else if (total >= 10) {
+//   // Console.WriteLine("You win a laptop!");
+//   } else if (total == 7) {
+//     // Console.WriteLine("You win a trip!");
+//   } else {
+//     // Console.WriteLine("You win a kitten!");
+//   }
+
+
+// ----  Complete a challenge activity to apply business rules
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+  Console.WriteLine(daysUntilExpiration);
+// Your code goes here
+// daysUntilExpiration = 11;
+if (daysUntilExpiration == 0) {
+  Console.WriteLine("Your subscription has expired.");
+} else if (daysUntilExpiration == 1) {
+  discountPercentage = 20;
+  Console.WriteLine($"Your subscription expires within a day!");
+} else if (daysUntilExpiration <= 5) {
+  discountPercentage = 10;
+  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+} else if (daysUntilExpiration <= 10) {
+  Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-if (total >= 16) {
-  Console.WriteLine("You win a car!");
-  } else if (total >= 10) {
-  Console.WriteLine("You win a laptop!");
-  } else if (total == 7) {
-    Console.WriteLine("You win a trip!");
-  } else {
-    Console.WriteLine("You win a kitten!");
-  }
+if (discountPercentage > 0) {
+  Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
