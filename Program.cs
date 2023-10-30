3522d142757fb20cdc3f37b39c64258e5d060e5e
Favio Valero <favio102@protonmail.com>
2023-10-29 21:16:32 -0700

    call methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(1);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+// Console.WriteLine(Int32.MinValue);
+// Console.WriteLine(Int32.MaxValue);
+
+// unit 5 0f 8
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
