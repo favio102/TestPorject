693646a79c725073c89e6596a66e42f3a8d9f2cb
Favio Valero <favio102@protonmail.com>
2023-10-29 23:24:34 -0700

    Create nested decision logic with if, else if, and else

--- Program.cs
+++ Program.cs
@@ -12,8 +12,77 @@
 
 // unit 5 0f 8
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine(largerValue);
+// Console.WriteLine(largerValue);
+
+// -----------Decision logic game 1st version
+
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// // test the code
+// // roll1 = 4;
+// // roll2 = 6;
+// // roll3 = 5;
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+//   if ((roll1 == roll2) && (roll2 == roll3)) {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+//   } else {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+//   }
+// }
+
+// if (total >= 15) {
+//   Console.WriteLine($"Score: {total}\nYou win!");
+//   }
+// else {
+//   Console.WriteLine($"Score: {total}\nSorry, you lose.");
+// }
+// -----------Decision logic game 2nd version
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+// // test the code
+// roll1 = 4;
+// roll2 = 2;
+// roll3 = 1;
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+  if ((roll1 == roll2) && (roll2 == roll3)) {
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+  } else {
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+  }
+}
+
+if (total >= 16) {
+  Console.WriteLine("You win a car!");
+  } else if (total >= 10) {
+  Console.WriteLine("You win a laptop!");
+  } else if (total == 7) {
+    Console.WriteLine("You win a trip!");
+  } else {
+    Console.WriteLine("You win a kitten!");
+  }
