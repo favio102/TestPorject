affc8bfb43cdb59488da661a059343f3f8417062
Favio Valero <favio102@protonmail.com>
2023-11-02 00:10:33 -0700

    Store and iterate through sequences of data using Arrays and the foreach statement in C#

--- Program.cs
+++ Program.cs
@@ -90,24 +90,47 @@
 
 // ----  Complete a challenge activity to apply business rules
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-  Console.WriteLine(daysUntilExpiration);
-// Your code goes here
-// daysUntilExpiration = 11;
-if (daysUntilExpiration == 0) {
-  Console.WriteLine("Your subscription has expired.");
-} else if (daysUntilExpiration == 1) {
-  discountPercentage = 20;
-  Console.WriteLine($"Your subscription expires within a day!");
-} else if (daysUntilExpiration <= 5) {
-  discountPercentage = 10;
-  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-} else if (daysUntilExpiration <= 10) {
-  Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+//   Console.WriteLine(daysUntilExpiration);
+// // Your code goes here
+// // daysUntilExpiration = 11;
+// if (daysUntilExpiration == 0) {
+//   Console.WriteLine("Your subscription has expired.");
+// } else if (daysUntilExpiration == 1) {
+//   discountPercentage = 20;
+//   Console.WriteLine($"Your subscription expires within a day!");
+// } else if (daysUntilExpiration <= 5) {
+//   discountPercentage = 10;
+//   Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+// } else if (daysUntilExpiration <= 10) {
+//   Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
 
-if (discountPercentage > 0) {
-  Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}
+// if (discountPercentage > 0) {
+//   Console.WriteLine($"Renew now and save {discountPercentage}%!");
+// }
+
+
+
+
+// ----------------------------------------------------------
+// Store and iterate through sequences of data using Arrays and the foreach statement in C#
+
+/*string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";*/
+// fraudulentOrderIDs[3] = "D000";
+
+string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reasign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
