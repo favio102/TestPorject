ec5c34a1ac170b0704addcbee7e7fc7c9a88c3cb
Favio Valero <favio102@protonmail.com>
2023-12-27 05:55:01 -0800

    Complete a challenge activity to improve code readability

--- Program.cs
+++ Program.cs
@@ -204,7 +204,7 @@ foreach (var orderID in orderIDs)
 
 // Use whitespace to make your code easier to read
 
-Random dice = new Random();
+/* Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
@@ -226,3 +226,33 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
         total += 2;
     }
 }
+ */
+
+
+// Complete a challenge activity to improve code readability
+
+/*
+   This code reverses a message, counts the number of times
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
