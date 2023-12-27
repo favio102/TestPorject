1bcbeca26708b902c5e05f0f0c9321820d7c9c9a
Favio Valero <favio102@protonmail.com>
2023-12-27 05:40:57 -0800

    Use whitespace to make your code easier to read

--- obj/Debug/net7.0/TestPorject.AssemblyInfo.cs
+++ obj/Debug/net7.0/TestPorject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPorject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bee695cb19f03a0658f542ce19c07a3b0a4dc62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc638af99097c51b862d80b37bf57b3d0fb388a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPorject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPorject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
