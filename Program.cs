dc638af99097c51b862d80b37bf57b3d0fb388a4
Favio Valero <favio102@protonmail.com>
2023-12-27 05:24:25 -0800

    Create effective code comments

--- Program.cs
+++ Program.cs
@@ -166,12 +166,38 @@ Console.WriteLine($"We have {sum} items in the inventory.");*/
 
 // ----------------------------------------------
 // Complete a challenge activity for nested iteration and selection statements
-// ------------------------------------------
 
-string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+/*string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 foreach (string orderID in orderIDs)
 {
   if (orderID.StartsWith("B")){
     Console.WriteLine($"Potentially fraudulent Order IDs are: {orderID}");
   }
+}*/
+
+
+// ----------------------------------------------
+// Create readable code with conventions, whitespace, and comments in C#
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+ */
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
 }
--- obj/Debug/net7.0/TestPorject.AssemblyInfo.cs
+++ obj/Debug/net7.0/TestPorject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPorject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df69fd88edbe2b67bb2282133858478309c1e341")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bee695cb19f03a0658f542ce19c07a3b0a4dc62")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPorject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPorject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
